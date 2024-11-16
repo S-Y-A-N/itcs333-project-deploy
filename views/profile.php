2d7bdeadcdbff96eda36f2c6e7e88e3cca54579e
Intisar <202107996@stu.uob.edu.bh>
2024-11-16 17:36:37 +0300

    blue bar

---
+++ views/profile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<style>
+#blue bar{
+    hight: 50px;
+    background-color: #405d9b;
+    color: #d9dfeb
+}
+
+</style>
+
+<body>
+   
+<div id="blue bar">
+
+</div>
+
+
+
+</body>
+</html>
