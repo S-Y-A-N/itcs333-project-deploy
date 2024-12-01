63b493c6b79ce611c31b699143efcab52b8eb63f
SYAN <syanays.git@gmail.com>
2024-12-01 10:59:37 +0300

    update

--- itcs333_project.sql
+++ itcs333_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 30, 2024 at 02:16 PM
+-- Generation Time: Dec 01, 2024 at 08:58 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,25 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `s40_rooms`
+-- Table structure for table `bookings`
 --
 
-CREATE TABLE `s40_rooms` (
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `booking_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `email`, `room_id`, `booking_time`) VALUES
+(1, '200@uob.edu.bh', 1020, '2024-12-01 10:03:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
   `room_id` int(11) NOT NULL,
   `room_name` varchar(256) DEFAULT NULL,
   `type` enum('classroom','lab') NOT NULL,
   `dept` enum('is','cs','ce') NOT NULL,
-  `capacity` int(11) NOT NULL
+  `capacity` int(11) NOT NULL,
+  `usage` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `s40_rooms`
+-- Dumping data for table `rooms`
 --
 
-INSERT INTO `s40_rooms` (`room_id`, `room_name`, `type`, `dept`, `capacity`) VALUES
-(1020, NULL, 'classroom', 'is', 40),
-(1048, NULL, 'lab', 'cs', 40),
-(1080, NULL, 'classroom', 'ce', 40);
+INSERT INTO `rooms` (`room_id`, `room_name`, `type`, `dept`, `capacity`, `usage`) VALUES
+(1020, NULL, 'classroom', 'is', 40, 0),
+(1048, NULL, 'lab', 'cs', 40, 0),
+(1080, NULL, 'classroom', 'ce', 40, 0);
 
 -- --------------------------------------------------------
 
@@ -52,26 +73,54 @@ INSERT INTO `s40_rooms` (`room_id`, `room_name`, `type`, `dept`, `capacity`) VAL
 
 CREATE TABLE `users` (
   `email` varchar(320) NOT NULL,
-  `password` varchar(256) NOT NULL
+  `password` varchar(256) NOT NULL,
+  `admin` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`email`, `password`) VALUES
-('100@uob.edu.bh', '$2y$10$KrYz0DBIadbKQqyfpA7tbeeTew3bbYZYQieSWYn4DcRRgOt4h4Zl6'),
-('200@uob.edu.bh', '$2y$10$DSwi4LYc1iDtFBXz0ziAXuyzCP3E2M3uBmoxZfzDxsYwx7NeGXGZq');
+INSERT INTO `users` (`email`, `password`, `admin`) VALUES
+('100@uob.edu.bh', '$2y$10$KrYz0DBIadbKQqyfpA7tbeeTew3bbYZYQieSWYn4DcRRgOt4h4Zl6', 0),
+('200@uob.edu.bh', '$2y$10$DSwi4LYc1iDtFBXz0ziAXuyzCP3E2M3uBmoxZfzDxsYwx7NeGXGZq', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `s40_rooms`
+-- Indexes for table `bookings`
 --
-ALTER TABLE `s40_rooms`
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
   ADD PRIMARY KEY (`room_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
