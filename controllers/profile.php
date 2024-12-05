a4e82a3ac8ca5f918076290d6f7678d74ae2175d
SYAN <syanays.git@gmail.com>
2024-12-05 15:20:00 +0300

    resolve conflict

--- controllers/profile.php
+++ controllers/profile.php
@@ -50,63 +50,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-use Core\Validator;
-
-authorize(isset($_SESSION['email']));
-
-use Core\Database;
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
-$currentEmail = $_SESSION['email'];
-$stmt = $db->query('SELECT * FROM users WHERE email = :currentEmail', [
-    'currentEmail' => $currentEmail
-]);
-$user = $stmt->fetch();
-
-$errors = [];
-
-$profilePicture = $user['profile_picture'];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-
-    if (Validator::uob_email($email)) {
-        try {
-
-            if (isset($_FILES['profile_picture']['name'])) {
-                unlink(base_path("public/{$_SESSION['pfp']}"));
-                unlink(base_path("uploads/{$_SESSION['pfp']}"));
-                $profilePicture = $username . "_" . basename($_FILES['profile_picture']['name']);
-                $_SESSION['pfp'] = $profilePicture;
-                move_uploaded_file($_FILES['profile_picture']['tmp_name'], base_path("uploads/$profilePicture"));
-                copy(base_path("uploads/$profilePicture"), base_path("public/$profilePicture"));
-            }
-    
-            $stmt = $db->query("UPDATE users SET username = :username, email = :email, profile_picture = :profile_picture WHERE email = :currentEmail", [
-                'username' => $username,
-                'email' => $email,
-                'profile_picture' => $profilePicture,
-                'currentEmail' => $currentEmail
-            ]);
-    
-            
-            $_SESSION['email'] = $email;
-    
-            $errors['message'] = "Profile updated successfully!";
-        }
-
-        catch(PDOException $e) {
-            echo $e->getMessage();
-        }
-  
-    } else {
-        $errors['email'] = "Invalid email format. Only UoB emails are allowed.";
-    }
-}
-
 view('profile.view.php', [
     'h1' => 'Profile',
     'p' => 'Customize your profile here',
